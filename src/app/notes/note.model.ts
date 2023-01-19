6bc2c4ee675099e327be87435d7ae867f0afa6ce
James Pfleiderer <jpfleiderer@gmail.com>
2023-01-19 15:56:14 -0500

    add createdDate to note model

--- src/app/notes/note.model.ts
+++ src/app/notes/note.model.ts
@@ -4,13 +4,15 @@ export class Note {
   public duration?: string;
   public startDateTime?: Date;
   public endDateTime?: Date;
+  public createdDateTime?: Date;
 
-  constructor(noteType: string, description: string, duration?: string, startDateTime?: Date, endDateTime?: Date){
+  constructor(noteType: string, description: string, duration?: string, startDateTime?: Date, endDateTime?: Date, createdDateTime?: Date){
     this.noteType = noteType;
     this.description = description;
     this.duration = duration;
     this.startDateTime = startDateTime;
     this.endDateTime = endDateTime;
+    this.createdDateTime = createdDateTime;
   }
 
 }
