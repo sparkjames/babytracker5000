b65598b1d2232476004aa00a9f1ec0e3adb3f92d
James Pfleiderer <jpfleiderer@gmail.com>
2023-03-18 14:06:02 -0400

    update to use firebase to store notes

--- src/app/notes/note-storage.service.ts
+++ src/app/notes/note-storage.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { NotesService } from './notes.service';
-// import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Note } from './note.model';
-// import { BehaviorSubject, catchError, map, tap } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, tap, Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class NoteStorageService {
 
   isFetching = new BehaviorSubject<boolean>(false);
   isStoring = new BehaviorSubject<boolean>(false);
-  // private APIEndpoint = 'https://babytracker5000-default-rtdb.firebaseio.com/notes.json';
+  private APIEndpoint = 'https://babytracker5000-default-rtdb.firebaseio.com/notes.json';
 
   constructor(
-    // private http: HttpClient,
+    private http: HttpClient,
     private notesService: NotesService
-  ) {
-    // this.fetchNotes();
-  }
+  ) {}
 
   storeNotes(){
     // console.log('About to store notes...');
@@ -27,17 +25,20 @@ export class NoteStorageService {
     this.isStoring.next(true);
 
     try {
-      localStorage.setItem('notes', JSON.stringify(notes));
-      // this.http
-      //   .put(
-      //     this.APIEndpoint,
-      //     notes
-      //   )
-      //   .subscribe(response => {
-      //     console.log('Notes stored.');
-      //     console.log(response);
-      //     this.isStoring.next(false);
-      //   });
+      // LocalStorage method
+      // localStorage.setItem('notes', JSON.stringify(notes));
+
+      // Remote method
+      this.http
+        .put(
+          this.APIEndpoint,
+          notes
+        )
+        .subscribe(response => {
+          console.log('Notes stored.');
+          console.log(response);
+          this.isStoring.next(false);
+        });
 
     } catch (error) {
       console.error('Error fetching notes: ', error);
@@ -48,45 +49,50 @@ export class NoteStorageService {
   }
 
   fetchNotes() {
-    // console.log('start fetch');
+    console.log('start fetch');
     this.isFetching.next(true);
 
-    const notes:Note[] = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes') || '{}') : [];
-    if (notes) {
-      this.notesService.setNotes(notes);
+    // LocalStorage method
+    // const notes:Note[] = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes') || '{}') : [];
+    // if (notes) {
+    //   this.notesService.setNotes(notes);
+    // }
+    // this.isFetching.next(false);
+
+    // return notes;
+
+    // Remote method
+    return this.http.get(this.APIEndpoint).subscribe( notes => {
+      // console.log('fetched notes: ');
+      // console.log(typeof notes);
+      // console.log(notes);
+
+      // const newNotes = Object.values(notes);
+      const newNotes: Note[] = [];
+      Object.values(notes).forEach( note => newNotes.push(note) );
+      // console.log('newNotes = ', newNotes);
+
+      if( newNotes ){
+        this.notesService.setNotes(newNotes);
+      }
+      this.isFetching.next(false);
+      // console.log('finished fetch');
+      // console.log(notes);
+    }, this.handleError)
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
     }
-    this.isFetching.next(false);
-    // console.log('finished fetch');
-
-    return notes;
-
-    // return this.http
-    //   .get<Note[]>(this.APIEndpoint)
-    //   .pipe(
-    //     map( notes => {
-    //       console.log(notes);
-    //       return notes.map( note => {
-    //         return {
-    //           ...note,
-    //           feedDetails: note.feedDetails ? note.feedDetails : '',
-    //           diaperDetails: note.diaperDetails ? note.diaperDetails : ''
-    //         }
-    //       })
-    //     }),
-    //     tap( notes => {
-    //       if( notes ){
-    //         this.notesService.setNotes(notes);
-    //       }
-    //       this.isFetching.next(false);
-    //       console.log('finished fetch');
-    //       console.log(notes);
-    //     }),
-    //     catchError((error, caught) => {
-    //       console.log('Error in fetching note list: ', error);
-    //       this.isFetching.next(false);
-    //       return [];
-    //     })
-    //   );
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 
 }
