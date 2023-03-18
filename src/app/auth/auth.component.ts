d57693a91d2094ef055dd9591301abeb0dc115cb
James Pfleiderer <jpfleiderer@gmail.com>
2023-03-18 16:18:16 -0400

    setting up form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  authMode = 'login';
+
+  onSwitchMode(){
+    if( this.authMode === 'login' ){
+      this.authMode = 'register';
+    } else {
+      this.authMode = 'login';
+    }
+  }
+
+  onSubmit( form: NgForm ){
+    console.log(form.value);
+    form.reset();
+  }
 
 }
