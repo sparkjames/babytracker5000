4e3feebaca681cfd8b96833401db271a19bff6de
James Pfleiderer <jpfleiderer@gmail.com>
2023-03-20 14:34:27 -0400

    add guard to redirect away from notes if not logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { NotesListComponent } from './notes/notes-list/notes-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
-  { path: 'notes', component: NotesListComponent }
+  {
+    path: 'notes',
+    component: NotesListComponent,
+    canActivate: [AuthGuard],
+  }
   // account
 ];
 
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { map, Observable, take } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  constructor ( private authService: AuthService, private router: Router ){}
+
+  // If not logged in, redirect from the notes listing page to the auth page.
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      take(1),
+      map( user => {
+        const isAuth = !!user;
+        if( isAuth ){
+          return true;
+        } else {
+          return this.router.createUrlTree(['/auth']);
+        }
+      })
+    );
+  }
+
+}
