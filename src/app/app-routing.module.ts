69360257cbfb9cbf8db6f5db61d1c3e85aa2f886
James Pfleiderer <jpfleiderer@gmail.com>
2023-04-04 14:30:56 -0400

    seperate guards for notes and auth pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/auth.guard';
 import { NotesListComponent } from './notes/notes-list/notes-list.component';
+import { NotesGuard } from './notes/notes.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    canActivate: [AuthGuard]
+  },
   {
     path: 'notes',
     component: NotesListComponent,
-    canActivate: [AuthGuard],
+    canActivate: [NotesGuard],
   }
   // account
 ];
--- src/app/notes/notes.guard.ts
+++ src/app/notes/notes.guard.ts
@@ -1,20 +1,25 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { map, Observable, take } from "rxjs";
-import { AuthService } from "./auth.service";
+import { AuthService } from "../auth/auth.service";
 
-@Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+@Injectable({ providedIn: 'root' })
+export class NotesGuard implements CanActivate {
 
-  constructor ( private authService: AuthService, private router: Router ){}
+  constructor(private authService: AuthService, private router: Router) { }
 
   // If not logged in, redirect from the notes listing page to the auth page.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     return this.authService.user.pipe(
       take(1),
-      map( user => {
-        return (!user || user?.id === 'offline') ? true : false;
+      map(user => {
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        } else {
+          return this.router.createUrlTree(['/auth']);
+        }
       })
     );
   }
